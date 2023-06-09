0f3197ac2974d69468f0d4a7e9b84efac3f96c39
soujanyamh <136050858+soujanyamh@users.noreply.github.com>
2023-06-09 16:46:50 +0530

    Update dataprep.py

--- Regression_Retail/dataprep.py
+++ Regression_Retail/dataprep.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 # Checking for null values
 bike.isnull().sum()
 
@@ -15,6 +18,17 @@ bike['season']=bike.season.map({1: 'spring', 2: 'summer',3:'fall', 4:'winter'})
 bike['month']=bike.month.map({1:'Jan',2:'Feb',3:'Mar',4:'Apr',5:'May',6:'June',7:'July',8:'Aug',9:'Sep',10:'Oct',11:'Nov',12:'Dec'})
 bike['weathersit']=bike.weathersit.map({1: 'Clear',2:'Mist + Cloudy',3:'Light Snow',4:'Snow + Fog'})
 bike['weekday']=bike.weekday.map({0:'Sun',1:'Mon',2:'Tue',3:'Wed',4:'Thu',5:'Fri',6:'Sat'})
+
+# Combine the new dummy variables with the original dataframe
+bike = pd.concat([months, weekdays, weather_sit, seasons, bike], axis=1)
+
+# Drop the original categorical variables
+bike.drop(['season', 'month', 'weekday', 'weathersit'], axis=1, inplace=True)
+
+# Check the final shape of the dataset
+print(bike.shape)
+
+# Preview the processed dataset
 bike.head()
 
 
