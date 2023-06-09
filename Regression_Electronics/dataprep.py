96f4346db78b574d847fea770ab7c8f134d3ec45
soujanyamh <136050858+soujanyamh@users.noreply.github.com>
2023-06-09 14:20:21 +0530

    Update dataprep.py

--- Regression_Electronics/dataprep.py
+++ Regression_Electronics/dataprep.py
@@ -1,1 +1,17 @@
+### **Data Checks**
+- Checking missing values
+- Checking duplicates
+- Checking data types of each column
+- Checking the number of unique values of each column
+- Checking statistics of data set
+- Checking various categories present in different categorical column
+
+df.isna().sum()
+
+df.duplicated().sum()
+
+df = df.drop_duplicates()
+
+df.nunique()
+
 
