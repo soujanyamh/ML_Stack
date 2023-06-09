457709f159c0e9ea4dab54fa46fb526dace50b13
soujanyamh <136050858+soujanyamh@users.noreply.github.com>
2023-06-09 16:39:19 +0530

    Update dataprep.py

--- Regression_Business/dataprep.py
+++ Regression_Business/dataprep.py
@@ -1,1 +1,15 @@
+import pandas as pd
+
+# Read the dataset
+df = pd.read_csv('/kaggle/input/advertising-sales-dataset/Advertising Budget and Sales.csv')
+
+# Drop the 'Unnamed: 0' column
+df.drop('Unnamed: 0', axis=1, inplace=True)
+
+# Rename the columns
+df.columns = ['TV', 'Radio', 'Newspaper', 'Sales']
+
+# Split the data into input features (A) and target variable (B)
+A = df[['TV', 'Radio', 'Newspaper']]
+B = df['Sales']
 
