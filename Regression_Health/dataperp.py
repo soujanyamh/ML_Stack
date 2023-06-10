3d2ffebe4c71d3d13bdcbfaa71fe65df5087c2ce
soujanyamh <136050858+soujanyamh@users.noreply.github.com>
2023-06-10 12:24:13 +0530

    Update dataperp.py

--- Regression_Health/dataperp.py
+++ Regression_Health/dataperp.py
@@ -1,2 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
 
-df = df.dropna(how='any',axis=0) 
+# Reading the Dataset
+df = pd.read_csv('/kaggle/input/student-mental-health/Student Mental health.csv')
+
+# Dropping rows with missing values
+df = df.dropna(how='any', axis=0)
+
+# Renaming column
+df.rename(columns={'Choose your gender': 'gender'}, inplace=True)
+
+# Data visualization code goes here (not part of data preprocessing)
+
+# Data preprocessing code ends here
