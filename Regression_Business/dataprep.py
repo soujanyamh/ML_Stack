0bed212ee7a5a0884d8f6bc6f1e3fb47990ab95b
soujanyamh <136050858+soujanyamh@users.noreply.github.com>
2023-06-10 12:16:52 +0530

    Update dataprep.py

--- Regression_Business/dataprep.py
+++ Regression_Business/dataprep.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# Read the dataset
+# Load the dataset
 df = pd.read_csv('/kaggle/input/advertising-sales-dataset/Advertising Budget and Sales.csv')
 
 # Drop the 'Unnamed: 0' column
@@ -9,7 +10,7 @@ df.drop('Unnamed: 0', axis=1, inplace=True)
 # Rename the columns
 df.columns = ['TV', 'Radio', 'Newspaper', 'Sales']
 
-# Split the data into input features (A) and target variable (B)
+# Split the data into training and testing sets
 A = df[['TV', 'Radio', 'Newspaper']]
 B = df['Sales']
-
+trainx, testx, trainy, testy = train_test_split(A, B, test_size=0.3, random_state=0)
